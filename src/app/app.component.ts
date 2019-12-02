9430b0fab168cf184a0324199d50ebd8a39703b2
Thomas Seignette <tseignette@pm.me>
2019-12-02 22:29:18 +0100

    Animation on navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { INFORMATION } from './INFORMATION';
+import { RouterOutlet } from '@angular/router';
+import { trigger, transition, query, style, animate, animateChild, group } from '@angular/animations';
+
+export const fader =
+  trigger('routeAnimations', [
+    transition('* <=> *', [
+      query(
+        ':enter',
+        [
+          style({ display: 'block', opacity: 0 }),
+          animate('0.4s ease', style({ opacity: 1 }))
+        ],
+        { optional: true }
+      ),
+    ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    fader
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -15,6 +34,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.titleService.setTitle(this.titleService.getTitle() + ' ' + INFORMATION.name);
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ThemeModule } from './@theme/theme.module';
   imports: [
     AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'portfolio' }),
+    BrowserAnimationsModule,
 
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
