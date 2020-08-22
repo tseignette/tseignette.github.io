0a4c0c8979eb22d0c163cbb3ef355a54e92bb8b2
Thomas Seignette <tseignette@pm.me>
2020-08-22 10:18:24 +0200

    Animation added on navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { INFORMATION } from './@core/INFORMATION.constants';
+import { trigger, transition, group, query, style, animate } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        group([
+          query(
+            ':enter',
+            [
+              style({ display: 'block', opacity: 0 }),
+              animate('0.3s ease', style({ opacity: 1 })),
+            ],
+            { optional: true }
+          )
+        ]),
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
 
   constructor(
+    public router: Router,
     private titleService: Title
   ) {
     this.titleService.setTitle(`${INFORMATION.name} / ${INFORMATION.job}`);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { ThemeModule } from './@theme/theme.module';
   ],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     CoreModule.forRoot(),
     ThemeModule.forRoot()
